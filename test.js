d7b648e6a602ee549bfd1219db681e176ad47df0
error4OA <99040365+error4OA@users.noreply.github.com>
2022-04-28 14:48:49 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,23 @@
+/**
+ * @name Example
+ * @version 0.0.1
+ * @description Example Description
+ * @author Example
+ *  
+ */
+
+
+
+ module.exports = class Example{
+     
+    load() { }
+    start() {
+
+BdApi.alert("hello","i saw u turning this plugin on, its completly useless so please turn off")
+
+//stop coding
+}
+stop(){
+
+}
+}
